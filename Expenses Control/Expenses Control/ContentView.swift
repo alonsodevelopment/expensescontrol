ce6ffaaaa3d263c6d231368aaee237b87e542460
Alonso Nuñez <alonso.nunez.development@gmail.com>
2021-07-23 16:00:31 -0600

    first commit

---
+++ Expenses Control/Expenses Control/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Expenses Control
+//
+//  Created by Alonso Nuñez on 23/07/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Expenses Control/Expenses Control/Expenses_ControlApp.swift
@@ -0,0 +1,17 @@
+//
+//  Expenses_ControlApp.swift
+//  Expenses Control
+//
+//  Created by Alonso Nuñez on 23/07/21.
+//
+
+import SwiftUI
+
+@main
+struct Expenses_ControlApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
